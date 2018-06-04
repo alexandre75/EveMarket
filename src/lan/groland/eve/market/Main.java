885c4a5a96460b8e7a020c8940a83e84570b3edd
alexandre <alex04@cuenca.me>
2018-06-03 23:31:19 -0700

    using mongodb

--- src/lan/groland/eve/market/Main.java
+++ src/lan/groland/eve/market/Main.java
@@ -1,17 +1,16 @@
 package lan.groland.eve.market;
 
 import io.swagger.client.ApiException;
+import io.swagger.client.api.UniverseApi;
+import io.swagger.client.model.GetUniverseTypesTypeIdOk;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.NumberFormat;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -19,14 +18,23 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
 public class Main {
 
 	private static Logger log = Logger.getLogger("Main");
-	
+
 	final public static int DODIXIE = 30002659;
 	final public static int AMARR = 30002187;
 	final public static int HEK = 30002053;
-	
+
 	public final static int SINQ_SAISON = 10000032;
 	final public static int DOMAIN = 10000043;
 	final public static int METROPOLIS = 10000042;
@@ -35,7 +43,7 @@ public class Main {
 	final public static int CURSE = 10000012;
 	final public static int THE_CITADEL = 10000033;
 	final public static int ESOTERIA = 10000039;
-	
+
 	final public static long[] D_P = {1024004680659l};
 	final public static long[] DODIXIE_STATION = {60011866};
 	final public static long[] AMARR_STATION = {60008494};
@@ -43,11 +51,19 @@ public class Main {
 	final public static long[] RENS_STATION = {60004588};
 	final public static long[] BRAVELAND_STATION = {61000182,1023729674815l};
 	final public static long[] TAMA = {60005203};
+
+	private static Set<Integer> alreadyBought;
+
+	static UniverseApi univers = new UniverseApi();
+
+	final static int cargo = 320000;
+	final static int region = ESOTERIA;
+	final static  float fee = .962f; //.95f;
+	final static int PLACE = 20; 
+	final static double cash = 2500e6;
+	final static BestTrades trades = new BestTrades(PLACE, cargo); 
 	
-	final private static NumberFormat cash = NumberFormat.getIntegerInstance();
-	{
-		cash.setGroupingUsed(true);
-	}
+	static MongoCollection<Document> itemDescritions;
 	
 	/**
 	 * @param args
@@ -58,15 +74,16 @@ public class Main {
 	 */
 	public static void main(String[] args) throws SQLException, IOException, InterruptedException, ApiException {
 		final long[] station = D_P;
-		final int region = ESOTERIA;
-		final double cash = 3e9;
-		final int PLACE = 20; 
-		final int cargo = 320000;
-		final float fee = .962f; //.95f;
 		
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://jupiter:27017"));
+		MongoDatabase eve = mongoClient.getDatabase("Eve");
+		itemDescritions = eve.getCollection("Items");
+
 		OrdersStatsBuilder ordersStats = OrdersStatsBuilder.newInstance(station, region, false);
-		
-		Set<Integer> alreadyBought = new HashSet<Integer>();
+
+		OrdersStatsBuilder jita = OrdersStatsBuilder.newInstance(new long[]{60003760}, 10000002, true);
+
+		alreadyBought = new HashSet<Integer>();
 		try (BufferedReader reader = new BufferedReader(new FileReader("orders"))){
 			reader.readLine();
 			String l;
@@ -77,89 +94,101 @@ public class Main {
 				}
 			}
 		}
-		 Connection conn = DriverManager.getConnection(
-	               "jdbc:mysql://192.168.45.112:3306/eve", "alex", "chHfn8Zc");
-	     //  is returned in a "ResultSet" object.
-         String strSelect = "select eve_inv_types.type_id, eve_inv_types.name, eve_inv_types.jita_price_sell, volume " +
-         		"from eve_inv_types " +
-         		"where eve_inv_types.jita_price_sell <> 0 and eve_inv_types.jita_price_sell < "+(cash/(float)10)//+" and eve_inv_types.type_id = 22553 "
-         				;
-         //		+" having count(items_history.quantity) / 27.0 > 0.6; ";
-         log.fine("The SQL query is: " + strSelect); // Echo For debugging
-         System.out.println(strSelect);
- 
-         Statement stmt = conn.createStatement();
-         ResultSet rset = stmt.executeQuery(strSelect);
-         
-         final BestTrades trades = new BestTrades(PLACE, cargo); 
-         
-         System.out.println("Preselect done");
-         
-         ExecutorService executor = new ThreadPoolExecutor(10, 10, 0, TimeUnit.DAYS, new LinkedBlockingDeque<Runnable>());
- 
-         while(rset.next()) {   // Move the cursor to the next row
-            final String name = rset.getString("name");
-            final double buyPrice = rset.getDouble("jita_price_sell");
-            final int    id   = rset.getInt("type_id");
-            final double volume = rset.getDouble("volume");
-            log.fine(name + ", " + buyPrice + ", " + id);
-                   
-            if (volume > cargo) continue; // ne rentre pas dans le cargo
-            if (alreadyBought.contains(id)) continue;
-            
-            Runnable run = new Runnable(){
-            	public void run(){
-            		try {
 
-            		MarketStatistics.Sales sales = MarketStatistics.getInstance().medianPrice(id, region, buyPrice);
+		
 
-            		double sellPrice = sales.price;
-            		double quantitéJounalière = sales.quantity;
+		ExecutorService executor = new ThreadPoolExecutor(10, 10, 0, TimeUnit.DAYS, new LinkedBlockingDeque<Runnable>());
+		
+		
+		Iterator<Integer> items =  jita.cheaperThan(cash/10);
+		while(items.hasNext()) {   // Move the cursor to the next row
+			final int id = items.next();
+			OrderStats jitaP = jita.get(id);
+			OrderStats orders = ordersStats.get(id);
+			Runnable run = new Command(id, jitaP, orders);
+			executor.execute(run);
+		}
+		executor.shutdown();
+		executor.awaitTermination(5, TimeUnit.HOURS);
+		System.out.println(trades.multiBuyString());
+		System.out.println(trades.toString());
 
-            		if (quantitéJounalière < 1) return ; // il faut au moins en vendre 1 par jour
-
-
-            		// On n'accepte pas les rentabilités historiques < 20%
-            		if (sellPrice * .975f /*taxe*/ -buyPrice <= .20 * buyPrice) return; 
-
-            		
-            		
-            		OrderStats orders = ordersStats.get(id);
-            		if (orders == null) return;
-            		
-            		// Si pas de concurence, on tente la culbute
-            		double prixDeVente = ((orders.getBid() < Float.MAX_VALUE)? orders.getBid() : buyPrice*1.5);
-            		prixDeVente = Math.min(1.5*sellPrice, prixDeVente); // on ne vendra probablement pas à n'importe quel prix
-            		double margeUnitaire =  prixDeVente * fee /*taxe*/ -buyPrice;
-
-            		if (prixDeVente/buyPrice < 1.40f) return; // pas de rentabilité < 20% sinon on pourrait être en perte
-
-            		double quantiteAAcheter = Math.ceil(quantitéJounalière/(double)(orders.nbSellOrders() +1));
-            		//if (orders.nbSellOrders() <= 1) quantiteAAcheter *= 2;
-
-            		double margeProbable = Math.min(quantitéJounalière/(double)(orders.nbSellOrders() +1), quantiteAAcheter)*margeUnitaire;
-
-
-            		Trade trade = new Trade(new Item(id, name, volume), margeProbable, quantiteAAcheter, prixDeVente, buyPrice);
-            		if (margeProbable < 1e6) return; // on ne se baisse plus pour 3 fois rien
-            		if (trade.ajust(cash/PLACE)){
-            			// System.out.println(name + "\t : " + cash.format(margeParTrader) + "\t" + volume);
-            			trades.add(trade);
-            		}
-            		} catch(Exception e){
-            			log.severe(e.getLocalizedMessage());
-            			e.printStackTrace(System.out);
-            			System.exit(1);
-            		}
-            	}
-            };
-            executor.execute(run);
-         }
-         executor.shutdown();
-         executor.awaitTermination(5, TimeUnit.HOURS);
-         System.out.println(trades.multiBuyString());
-         System.out.println(trades.toString());
-         
 	}
+
+	static class Command implements Runnable {
+		public Command(int id, OrderStats os , OrderStats orders){
+			this.id = id;
+			this.orderStats = os;
+			this.orders = orders;
+		}
+
+		private int id;
+		private OrderStats orderStats;
+		OrderStats orders;
+
+
+		public void run(){        	
+
+			if (orderStats == null){
+				return;
+			}
+			if (orders == null) return;
+			try {
+				BasicDBObject query = new BasicDBObject("id", id);
+				Document res = itemDescritions.find(query).first();
+				if (res == null){
+					GetUniverseTypesTypeIdOk info = univers.getUniverseTypesTypeId(id, null, null, null, null, null);
+
+					
+					res = new Document("id", id)
+					.append("volume", new Double(info.getVolume()))
+					.append("name", info.getName())
+					.append("timestamp", new Date());
+					itemDescritions.insertOne(res);	
+				} 
+				
+				final double buyPrice = orderStats.getBid();
+				final double volume = res.getDouble("volume");
+				if (volume > cargo) return; // ne rentre pas dans le cargo
+				if (alreadyBought.contains(id)) return;
+
+
+				MarketStatistics.Sales sales = MarketStatistics.getInstance().medianPrice(id, region, buyPrice);
+
+				double sellPrice = sales.price;
+				double quantitéJounalière = sales.quantity;
+
+				if (quantitéJounalière < 1) return ; // il faut au moins en vendre 1 par jour
+
+
+				// On n'accepte pas les rentabilités historiques < 20%
+				if (sellPrice * .975f /*taxe*/ -buyPrice <= .20 * buyPrice) return; 
+
+				// Si pas de concurence, on tente la culbute
+				double prixDeVente = ((orders.getBid() < Float.MAX_VALUE)? orders.getBid() : buyPrice*1.5);
+				prixDeVente = Math.min(1.5*sellPrice, prixDeVente); // on ne vendra probablement pas à n'importe quel prix
+				double margeUnitaire =  prixDeVente * fee /*taxe*/ -buyPrice;
+
+				if (prixDeVente/buyPrice < 1.40f) return; // pas de rentabilité < 20% sinon on pourrait être en perte
+
+				double quantiteAAcheter = Math.ceil(quantitéJounalière/(double)(orders.nbSellOrders() +1));
+				//if (orders.nbSellOrders() <= 1) quantiteAAcheter *= 2;
+
+				double margeProbable = Math.min(quantitéJounalière/(double)(orders.nbSellOrders() +1), quantiteAAcheter)*margeUnitaire;
+
+				final String name = res.getString("name");
+				Trade trade = new Trade(new Item(id, name, volume), margeProbable, quantiteAAcheter, prixDeVente, buyPrice);
+				if (margeProbable < 1e6) return; // on ne se baisse plus pour 3 fois rien
+				if (trade.ajust(cash/PLACE)){
+					// System.out.println(name + "\t : " + cash.format(margeParTrader) + "\t" + volume);
+					trades.add(trade);
+				}
+			} catch(Exception e){
+				log.severe(e.getLocalizedMessage());
+				e.printStackTrace(System.out);
+				System.exit(1);
+			}
+		}
+	};
 
 }
--- src/lan/groland/eve/market/OrdersStatsBuilder.java
+++ src/lan/groland/eve/market/OrdersStatsBuilder.java
@@ -17,8 +17,11 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 
 import javax.json.Json;
 import javax.json.stream.JsonParser;
@@ -27,7 +30,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.threeten.bp.LocalDateTime;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -86,20 +88,28 @@ public class OrdersStatsBuilder {
 		int page = 1;
 		List<GetMarketsRegionIdOrders200Ok> orders;
 		do {
-			orders = market.getMarketsRegionIdOrders("sell", (Integer)region, null, null, page++, null, null, null);
-			System.out.println("Page : " + page);
-			for (GetMarketsRegionIdOrders200Ok order : orders){
-				if (stationIn && -1 == Arrays.binarySearch(stationId, order.getLocationId())) continue;	
+			while (true){
+				try {
+					orders = market.getMarketsRegionIdOrders("sell", (Integer)region, null, null, page, null, null, null);
+					page++;
+					System.out.println("Page : " + page);
+					for (GetMarketsRegionIdOrders200Ok order : orders){
+						if (stationIn && -1 == Arrays.binarySearch(stationId, order.getLocationId())) continue;	
 
-				OrderStats stat = stats.get(order.getTypeId());
-				if (stat == null){
-					stat = new OrderStats(0, Float.MAX_VALUE, 0, Float.MIN_VALUE);
-					stats.put(order.getTypeId(), stat);
-				}
-				if (order.isIsBuyOrder()){
-					stat.newBuy(order.getPrice());
-				} else {
-					stat.newSell(order.getPrice(), order.getIssued().toLocalDateTime());
+						OrderStats stat = stats.get(order.getTypeId());
+						if (stat == null){
+							stat = new OrderStats(0, Float.MAX_VALUE, 0, Float.MIN_VALUE);
+							stats.put(order.getTypeId(), stat);
+						}
+						if (order.isIsBuyOrder()){
+							stat.newBuy(order.getPrice());
+						} else {
+							stat.newSell(order.getPrice(), order.getIssued().toLocalDateTime());
+						}
+					}
+					break;
+				} catch(ApiException e){
+					System.out.println(e.getLocalizedMessage());
 				}
 			}
 		} while(!orders.isEmpty());
@@ -269,6 +279,48 @@ public class OrdersStatsBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public Iterator<Integer> cheaperThan(double d) {
+		final Iterator<Entry<Integer, OrderStats>>iter = stats.entrySet().iterator();
+		return new Iterator<Integer>(){
+			
+			private Integer next;
+			
+			private void skip(){
+				if (next != null){
+					return;
+				}
+				next = null;
+				while(iter.hasNext()){
+					Entry<Integer, OrderStats> entr = iter.next();
+					if (entr.getValue().getBid() < d){
+						next = entr.getKey();
+						break;
+					} 
+				}
+			}
+			
+			@Override
+			synchronized public boolean hasNext() {
+				skip();
+				return next != null;
+			}
+
+			@Override
+			synchronized public Integer next() {
+				if (next == null){
+					skip();
+				}
+				if (next == null){
+					throw new NoSuchElementException();
+				}
+				int res = next;
+				next = null;
+				return res;
+			}
+			
+		};
 	}
 }
 
