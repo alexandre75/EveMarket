daa30c681055d7c27d195509ddf51074eda88ec6
alexandre <acuenca@quotemedia.com>
2019-06-26 18:42:53 -0700

    always changes

---
+++ evecop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+LIB=/usr/share/java
+export CLASSPATH=$LIB/guice.jar:$LIB/guava.jar:$LIB/mongo-java-driver.jar:$LIB/javax.json-1.1.4.jar:$LIB/reactive-streams-flow-adapters-1.0.2.jar:$LIB/rxjava-2.2.9.jar:$LIB/gson.jar:$LIB/atinject-jsr330-api.jar:$LIB/aopalliance.jar:$LIB/amqp-client.jar:$LIB/reactive-streams-1.0.2.jar:$LIB/slf4j-api.jar
+export CLASSPATH=$LIB/EveMarket-0.0.1-SNAPSHOT.jar:$CLASSPATH
+#CLASSPATH=./build/libs/EveMarket-0.0.1-SNAPSHOT.jar:$CLASSPATH
+java lan.groland.eve.bootstrap.EveMarket
